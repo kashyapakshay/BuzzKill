fbf82262bc3174d34c27654dfa5f1bb4d52f6218
Akshay Kashyap <kashyapa@union.edu>
2017-08-20 18:53:08 +0200

    Added routes and handlers for classification requests.

--- server.py
+++ server.py
@@ -1,13 +1,31 @@
 import tornado.ioloop
 import tornado.web
 
+from classifier import Classifier
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('Testing GET Request')
+        self.write('Clickbait Classifier: Make POST request to /classify with text to classify.')
+
+class ClassificationHandler(tornado.web.RequestHandler):
+    def initialize(self, classifier):
+        self.classifier = classifier
+
+    def _is_clickbait(self, text):
+        return bool(self.classifier.classify(text))
+
+    def get(self, text):
+        self.write(str(_is_clickbait(text)))
+
+    def post(self):
+        self.write(str(_is_clickbait(text)))
 
 def make_app():
+    classifier = Classifier()
+
     return tornado.web.Application([
-        ('/', MainHandler),
+        (r'/', MainHandler),
+        (r'/classify/(*)', ClassificationHandler, dict(classifier=classifier))
     ])
 
 if __name__ == "__main__":
